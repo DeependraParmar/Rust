47b7ccf087050bc62b4a10287328528c857bb292
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-15 06:50:00 +0530

    Variables and more

---
+++ variables_03/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // let is_male:bool = true;
+    // let is_above_18: bool = true;
+
+    // if is_male{
+    //     println!("You are a male");
+    // }
+    // else{
+    //     println!("You are not a male");
+    // }
+    // if is_male && is_above_18{
+    //     println!("You are a legal male");
+    // }
+
+    let str:String = String::from("Hey there!!");
+    println!("String is: {}", str);
+
+    
+}
